ccac2991bdd732b2f172891eb73c7a35e42c48b7
Tilo <tilo@pop-os.localdomain>
2020-09-07 19:25:14 +0200

    Allows CORS requests - Adds Logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller("token")
 export class AppController {
   constructor(private readonly appService: AppService) { }
+  logger: Logger = new Logger('AppController', true);
 
   @Get()
+  @Header('Access-Control-Allow-Origin', '*')
   getRegistrationTokens() {
+    this.logger.log('getRegistrationTokens');
     return this.appService.getAllTokens();
   }
   @Post()
+  @Header('Access-Control-Allow-Origin', '*')
   pushRegistrationToken(@Body() body) {
+    this.logger.log('pushRegistrationToken, body:' + body);
     return this.appService.saveToken(body.token);
   }
 }
