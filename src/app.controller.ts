bbcc0121db0ac67fd6bb5b655d92a9acc6bea8e9
Tilo <tilo@pop-os.localdomain>
2020-09-07 19:45:15 +0200

    Adds more header and more logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,14 +8,18 @@ export class AppController {
 
   @Get()
   @Header('Access-Control-Allow-Origin', '*')
+  @Header("Access-Control-Allow-Methods", "GET")
+  @Header("Access-Control-Allow-Headers", "Origin, Methods, Content-Type")
   getRegistrationTokens() {
     this.logger.log('getRegistrationTokens');
     return this.appService.getAllTokens();
   }
   @Post()
   @Header('Access-Control-Allow-Origin', '*')
+  @Header("Access-Control-Allow-Methods", "POST")
+  @Header("Access-Control-Allow-Headers", "Origin, Methods, Content-Type")
   pushRegistrationToken(@Body() body) {
-    this.logger.log('pushRegistrationToken, body:' + body);
+    this.logger.log('pushRegistrationToken, body:' + JSON.stringify(body));
     return this.appService.saveToken(body.token);
   }
 }
