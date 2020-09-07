c363f85bc4d2e8b4e081f127127f54ff24f4d5a5
Tilo <tilo@pop-os.localdomain>
2020-09-07 20:06:25 +0200

    Adds Access-Control-Allow-Credentials Header

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,25 @@
 import { Controller, Get, Post, Body, Logger, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller("token")
+@Controller('token')
 export class AppController {
   constructor(private readonly appService: AppService) { }
   logger: Logger = new Logger('AppController', true);
 
   @Get()
   @Header('Access-Control-Allow-Origin', '*')
-  @Header("Access-Control-Allow-Methods", "GET")
-  @Header("Access-Control-Allow-Headers", "Origin, Methods, Content-Type")
+  @Header('Access-Control-Allow-Methods', 'GET')
+  @Header('Access-Control-Allow-Headers', 'Origin, Authorization, Methods, Content-Type')
+  @Header('Access-Control-Allow-Credentials', 'true')
   getRegistrationTokens() {
     this.logger.log('getRegistrationTokens');
     return this.appService.getAllTokens();
   }
   @Post()
   @Header('Access-Control-Allow-Origin', '*')
-  @Header("Access-Control-Allow-Methods", "POST")
-  @Header("Access-Control-Allow-Headers", "Origin, Methods, Content-Type")
+  @Header('Access-Control-Allow-Methods', 'POST')
+  @Header('Access-Control-Allow-Headers', 'Origin, Methods, Content-Type')
+  @Header('Access-Control-Allow-Credentials', 'true')
   pushRegistrationToken(@Body() body) {
     this.logger.log('pushRegistrationToken, body:' + JSON.stringify(body));
     return this.appService.saveToken(body.token);
