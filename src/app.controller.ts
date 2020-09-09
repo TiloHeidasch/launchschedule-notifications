a1d2f1dd8dbe6eef5b090308324b64678a3c3fa6
Tilo <tilo@pop-os.localdomain>
2020-09-09 19:59:51 +0200

    Removes Header decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,19 +7,11 @@ export class AppController {
   logger: Logger = new Logger('AppController', true);
 
   @Get()
-  @Header('Access-Control-Allow-Origin', '*')
-  @Header('Access-Control-Allow-Methods', 'GET')
-  @Header('Access-Control-Allow-Headers', 'Origin, Authorization, Methods, Content-Type')
-  @Header('Access-Control-Allow-Credentials', 'true')
   getRegistrationTokens() {
     this.logger.log('getRegistrationTokens');
     return this.appService.getAllTokens();
   }
   @Post()
-  @Header('Access-Control-Allow-Origin', '*')
-  @Header('Access-Control-Allow-Methods', 'POST')
-  @Header('Access-Control-Allow-Headers', 'Origin, Methods, Content-Type')
-  @Header('Access-Control-Allow-Credentials', 'true')
   pushRegistrationToken(@Body() body) {
     this.logger.log('pushRegistrationToken, body:' + JSON.stringify(body));
     return this.appService.saveToken(body.token);
