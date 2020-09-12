68533e676a0f67fced9711cfa7e915dd87f14eba
Tilo <tilo@pop-os.localdomain>
2020-09-12 18:46:53 +0200

    Adds automatic Notification logic

---
+++ checkNotifications.ts
@@ -0,0 +1,170 @@
+var https = require('https');
+
+function requestUrl(url, mappingCallBack, actionCallBack) {
+  https
+    .get(url, resp => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', chunk => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        const recieved = JSON.parse(data);
+        const mapped = mappingCallBack(recieved.results);
+        actionCallBack(mapped);
+      });
+    })
+    .on('error', err => {
+      console.log('Error: ' + err.message);
+    });
+}
+function sendNotificationsForUpcomingLaunches() {
+  requestUrl(
+    'https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json&limit=10',
+    mapLaunches,
+    sendNotificationsForMappedData,
+  );
+}
+function sendNotificationsForUpcomingEvents() {
+  requestUrl(
+    'https://ll.thespacedevs.com/2.0.0/event/upcoming/?format=json&limit=10',
+    mapEvents,
+    sendNotificationsForMappedData,
+  );
+}
+function mapLaunches(launches) {
+  return launches.map(launch => {
+    return {
+      id: launch.id,
+      title: launch.name,
+      date: launch.net,
+      image: launch.image,
+      type: 'launch',
+    };
+  });
+}
+function mapEvents(events) {
+  return events.map(event => {
+    return {
+      id: event.id,
+      title: event.name,
+      date: event.date,
+      image: event.feature_image,
+      type: 'event',
+    };
+  });
+}
+function sendNotificationsForMappedData(mappedData) {
+  mappedData.forEach(dataSet => {
+    const timeDiff = new Date(dataSet.date).valueOf() - new Date().valueOf();
+    const secondDiff = timeDiff / 1000;
+    const minuteDiff = secondDiff / 60;
+    const hourDiff = minuteDiff / 60;
+    const dayDiff = hourDiff / 24;
+    if (minuteDiff < 5) {
+      raiseNotification(dataSet, 'minute');
+    } else if (hourDiff < 1) {
+      raiseNotification(dataSet, 'hour');
+    } else if (dayDiff < 1) {
+      raiseNotification(dataSet, 'day');
+    } else if (dayDiff < 7) {
+      raiseNotification(dataSet, 'week');
+    }
+  });
+}
+function raiseNotification(dataSet, notificationType) {
+  const url =
+    'https://launchschedule-notifications.th105.de/interest/' +
+    dataSet.type +
+    dataSet.id +
+    '?notificationType=' +
+    notificationType;
+
+  https
+    .get(url, resp => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', chunk => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        const tokenArray = JSON.parse(data);
+        raiseNotificationForTokenArray(tokenArray, dataSet, notificationType);
+      });
+    })
+    .on('error', err => {
+      console.log('Error: ' + err.message);
+    });
+}
+function raiseNotificationForTokenArray(tokenArray, dataSet, notificationType) {
+  tokenArray.forEach(token => {
+    raiseNotificationForToken(token, dataSet, notificationType);
+  });
+}
+function raiseNotificationForToken(token, dataSet, notificationType) {
+  sendNotification(
+    token,
+    'Upcoming ' + dataSet.type + ' in a ' + notificationType,
+    dataSet.title,
+    dataSet.image,
+    dataSet.id,
+    dataSet.type,
+  );
+
+  // update token server
+  makePost(
+    'launchschedule-notifications.th105.de',
+    443,
+    '/interest/' + dataSet.type + dataSet.id + '/' + token,
+    JSON.stringify({
+      notificationType,
+    }),
+  );
+}
+function sendNotification(to, title, body, image, id, type) {
+  // create FCM data
+  const data = {
+    to,
+    notification: {
+      title,
+      body,
+      image,
+    },
+    data: {
+      id,
+      type,
+    },
+  };
+  // send to FCM
+  makePost('fcm.googleapis.com', 443, '/fcm/send', JSON.stringify(data));
+}
+function makePost(hostname, port, path, data) {
+  const options = {
+    hostname,
+    port,
+    path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      Authorization: 'key=' + process.env.FCM_TOKEN,
+    },
+  };
+
+  const req = https.request(options, res => {});
+
+  req.on('error', error => {
+    console.error(error);
+  });
+
+  req.write(data);
+  req.end();
+}
+sendNotificationsForUpcomingLaunches();
+sendNotificationsForUpcomingEvents();
