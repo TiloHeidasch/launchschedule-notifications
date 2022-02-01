5526ffd06e89fc67b96e8c4778e4a206e5cbfc75
Tilo Heidasch <tilo.heidasch@capgemini.com>
2022-02-01 16:28:51 +0100

    Implements new notification scheme

--- checkNotifications.ts
+++ checkNotifications.ts
@@ -1,6 +1,7 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function requestFromLl(url) {
+  console.log({ function: "requestFromLl", url });
   try {
     const response = await axios.get(url);
     return response.data.results;
@@ -10,90 +11,87 @@ async function requestFromLl(url) {
 }
 
 async function sendNotificationsForUpcomingLaunches() {
+  console.log({ function: "sendNotificationsForUpcomingLaunches" });
   const launches = await requestFromLl(
-    'https://ll.thespacedevs.com/2.1.0/launch/upcoming/?format=json&limit=10',
+    "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?format=json&limit=10"
   );
   const mappedLaunches = mapLaunches(launches);
   sendNotificationsForMappedData(mappedLaunches);
 }
 async function sendNotificationsForUpcomingEvents() {
+  console.log({ function: "sendNotificationsForUpcomingEvents" });
   const events = await requestFromLl(
-    'https://ll.thespacedevs.com/2.1.0/event/upcoming/?format=json&limit=10',
+    "https://lldev.thespacedevs.com/2.2.0/event/upcoming/?format=json&limit=10"
   );
   const mappedEvents = mapEvents(events);
   sendNotificationsForMappedData(mappedEvents);
 }
 function mapLaunches(launches) {
-  return launches.map(launch => {
+  console.log({ function: "mapLaunches", launches });
+  return launches.map((launch) => {
     const relatedTypeIds = [];
     if (launch.launch_service_provider) {
       relatedTypeIds.push({
-        type: 'agency',
-        id: launch.launch_service_provider.id + '',
+        type: "agency",
+        id: launch.launch_service_provider.id + "",
       });
     }
     if (launch.pad && launch.pad.location) {
       relatedTypeIds.push({
-        type: 'location',
-        id: launch.pad.location.id + '',
+        type: "location",
+        id: launch.pad.location.id + "",
       });
     }
     if (launch.pad) {
-      relatedTypeIds.push({ type: 'pad', id: launch.pad.id + '' });
+      relatedTypeIds.push({ type: "pad", id: launch.pad.id + "" });
     }
     if (launch.rocket && launch.rocket.configuration) {
       relatedTypeIds.push({
-        type: 'rocket',
-        id: launch.rocket.configuration.id + '',
+        type: "rocket",
+        id: launch.rocket.configuration.id + "",
       });
     }
     return {
-      id: launch.id + '',
+      id: launch.id + "",
       title: launch.name,
       date: launch.net,
       image: launch.image,
       status: launch.status.id,
-      type: 'launch',
+      type: "launch",
       relatedTypeIds,
     };
   });
 }
 function mapEvents(events) {
-  return events.map(event => {
+  console.log({ function: "mapEvents", events });
+  return events.map((event) => {
     const relatedTypeIds = [];
     if (event.spacestations[0]) {
       relatedTypeIds.push({
-        type: 'spacestation',
-        id: event.spacestations[0].id + '',
+        type: "spacestation",
+        id: event.spacestations[0].id + "",
       });
     }
     return {
-      id: event.id + '',
+      id: event.id + "",
       title: event.name,
       date: event.date,
       image: event.feature_image,
       status: -1,
-      type: 'event',
+      type: "event",
       relatedTypeIds,
     };
   });
 }
 function sendNotificationsForMappedData(mappedData) {
-  mappedData.forEach(dataSet => {
-    if (isStatusOkToSend(dataSet.status)) {
+  console.log({ function: "sendNotificationsForMappedData", mappedData });
+  mappedData.forEach((dataSet) => {
+    if (isStatusOkToSend(dataSet.status) || dataSet.type === "event") {
       const timeDiff = new Date(dataSet.date).valueOf() - new Date().valueOf();
       const secondDiff = timeDiff / 1000;
       const minuteDiff = secondDiff / 60;
-      const hourDiff = minuteDiff / 60;
-      const dayDiff = hourDiff / 24;
-      if (minuteDiff < 5) {
-        raiseNotification(dataSet, 'minute');
-      } else if (hourDiff < 1) {
-        raiseNotification(dataSet, 'hour');
-      } else if (dayDiff < 1) {
-        raiseNotification(dataSet, 'day');
-      } else if (dayDiff < 7) {
-        raiseNotification(dataSet, 'week');
+      if (minuteDiff <= 15) {
+        raiseNotification(dataSet);
       }
     }
   });
@@ -111,132 +109,45 @@ function isStatusOkToSend(status) {
       return false;
   }
 }
-async function raiseNotification(dataSet, notificationType) {
-  const tokens = [];
-  tokens.push(
-    ...(await getTokens(
-      dataSet.type,
-      dataSet.id,
-      notificationType,
-      dataSet.date,
-      undefined,
-      undefined,
-    )),
-  );
-  for (let index = 0; index < dataSet.relatedTypeIds.length; index++) {
-    const relatedTypeId = dataSet.relatedTypeIds[index];
-    tokens.push(
-      ...(await getTokens(
-        relatedTypeId.type,
-        relatedTypeId.id,
-        notificationType,
-        dataSet.date,
-        dataSet.type,
-        dataSet.id,
-      )),
-    );
-  }
-  const uniqueTokens = tokens.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-  });
-  if (uniqueTokens.length > 0) {
-    raiseNotificationForTokenArray(uniqueTokens, dataSet, notificationType);
-    markNotified(dataSet, notificationType, uniqueTokens);
-  }
-}
-function markNotified(dataSet, notificationType, tokenArray) {
-  tokenArray.forEach(token => {
-    dataSet.relatedTypeIds.forEach(relatedTypeId => {
-      // update token server
-      put(
-        'https://launchschedule-notifications.th105.de/notification/' +
-          '?type=' +
-          relatedTypeId.type +
-          '&id=' +
-          relatedTypeId.id +
-          '&token=' +
-          token +
-          '&period=' +
-          notificationType +
-          '&date=' +
-          dataSet.date +
-          '&relatedType=' +
-          dataSet.type +
-          '&relatedId=' +
-          dataSet.id,
-      );
-    });
-    // update token server
-    put(
-      'https://launchschedule-notifications.th105.de/notification/' +
-        '?type=' +
-        dataSet.type +
-        '&id=' +
-        dataSet.id +
-        '&token=' +
-        token +
-        '&period=' +
-        notificationType +
-        '&date=' +
-        dataSet.date,
-    );
-  });
-}
-
-async function getTokens(
-  type,
-  id,
-  notificationType,
-  date,
-  relatedInterestType,
-  relatedInterestId,
-) {
-  let url =
-    'https://launchschedule-notifications.th105.de/notification' +
-    '?type=' +
-    type +
-    '&id=' +
-    id;
-  const response = await axios.get(url);
-  const notifications = response.data;
-  const tokens = [];
-  notifications.forEach(notification => {
-    let relevant = isRelevant(
-      notification,
-      type,
-      id,
-      notificationType,
-      date,
-      relatedInterestType,
-      relatedInterestId,
-    );
-    if (relevant) {
-      tokens.push(notification.token);
-    }
+async function raiseNotification(dataSet) {
+  console.log({ function: "raiseNotification", dataSet });
+  let condition = "'" + dataSet.type + dataSet.id + "' in topics";
+  dataSet.relatedTypeIds.forEach((relatedTypeId) => {
+    condition =
+      condition +
+      " || '" +
+      relatedTypeId.type +
+      relatedTypeId.id +
+      "' in topics";
   });
 
-  return tokens;
-}
-
-function raiseNotificationForTokenArray(tokenArray, dataSet, notificationType) {
-  tokenArray.forEach(token => {
-    raiseNotificationForToken(token, dataSet, notificationType);
-  });
-}
-function raiseNotificationForToken(token, dataSet, notificationType) {
   sendNotification(
-    token,
-    'Upcoming ' + dataSet.type + ' in less than a ' + notificationType,
+    condition,
+    "Upcoming " +
+      dataSet.type +
+      " in less than 15 Minutes at " +
+      new Date(dataSet.date).toLocaleTimeString("de") +
+      " CET",
     dataSet.title,
     dataSet.image,
     dataSet.id,
-    dataSet.type,
+    dataSet.type
   );
 }
-function sendNotification(to, title, body, image, id, type) {
+
+function sendNotification(condition, body, title, image, id, type) {
+  console.log({
+    function: "sendNotification",
+    condition,
+    title,
+    body,
+    image,
+    id,
+    type,
+  });
   // create FCM data
   const data = {
-    to,
+    condition,
     notification: {
       title,
       body,
@@ -248,97 +159,24 @@ function sendNotification(to, title, body, image, id, type) {
     },
   };
   // send to FCM
-  makePost('https://fcm.googleapis.com/fcm/send', JSON.stringify(data));
+  makePost("https://fcm.googleapis.com/fcm/send", JSON.stringify(data));
 }
 async function makePost(url, data) {
+  console.log({ function: "makePost", url, data });
+
   try {
     const response = await axios.post(url, data, {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         //'Content-Length': data.length,
-        Authorization: 'key=' + process.env.FCM_TOKEN,
+        //Authorization: "key=" + process.env.FCM_TOKEN,
+        Authorization:
+          "key=AAAAkONouqA:APA91bE3-2SHlQFyYrKN7HbDOv2WAC9glZlbv0J-AnJ0P-8xWtHE0rI8ElWh7XUtCQjvDubf37GhP0iVyNOKEEYX-GO43dwnPzor50o3Onjz8YyRxi7r009YDxJeY1V2H-FkQ4ovs15m",
       },
     });
   } catch (error) {
     console.error(error.response.data);
   }
 }
-async function put(url) {
-  try {
-    const response = await axios.put(url);
-  } catch (error) {
-    console.error(error.response.data);
-  }
-}
-function isRelevant(
-  notification,
-  type,
-  id,
-  notificationType,
-  date,
-  relatedInterestType,
-  relatedInterestId,
-) {
-  let relevant = true;
-  notification.notified.forEach(notifiedElement => {
-    if (
-      notifiedElement.date === date &&
-      (!relatedInterestType || !relatedInterestId)
-    ) {
-      switch (notificationType) {
-        case 'week':
-          if (notifiedElement.period === 'week') {
-            relevant = false;
-          }
-        case 'day':
-          if (notifiedElement.period === 'day') {
-            relevant = false;
-          }
-        case 'hour':
-          if (notifiedElement.period === 'hour') {
-            relevant = false;
-          }
-        case 'minute':
-          if (notifiedElement.period === 'minute') {
-            relevant = false;
-          }
-          break;
-        default:
-          break;
-      }
-    }
-    if (
-      notifiedElement.date === date &&
-      relatedInterestType &&
-      relatedInterestId &&
-      notifiedElement.relatedInterest &&
-      notifiedElement.relatedInterest.id === relatedInterestId &&
-      notifiedElement.relatedInterest.type === relatedInterestType
-    ) {
-      switch (notificationType) {
-        case 'week':
-          if (notifiedElement.period === 'week') {
-            relevant = false;
-          }
-        case 'day':
-          if (notifiedElement.period === 'day') {
-            relevant = false;
-          }
-        case 'hour':
-          if (notifiedElement.period === 'hour') {
-            relevant = false;
-          }
-        case 'minute':
-          if (notifiedElement.period === 'minute') {
-            relevant = false;
-          }
-          break;
-        default:
-          break;
-      }
-    }
-  });
-  return relevant;
-}
 sendNotificationsForUpcomingLaunches();
-sendNotificationsForUpcomingEvents();
+// sendNotificationsForUpcomingEvents();
